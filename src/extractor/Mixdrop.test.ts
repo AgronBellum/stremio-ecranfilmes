5e3b50abbba0ad700dba89ba2b35b6667087da60
AgronBellum <contato.deivid07@gmail.com>
2026-04-15 02:11:43 -0300

    Update Mixdrop.test.ts

--- src/extractor/Mixdrop.test.ts
+++ src/extractor/Mixdrop.test.ts
@@ -4,17 +4,27 @@ import { FetcherMock } from '../utils';
 import { ExtractorRegistry } from './ExtractorRegistry';
 import { Mixdrop } from './Mixdrop';
 
+
 const logger = winston.createLogger({ transports: [new winston.transports.Console({ level: 'nope' })] });
-const extractorRegistry = new ExtractorRegistry(logger, [new Mixdrop(new FetcherMock(`${__dirname}/__fixtures__/Mixdrop`))]);
+const extractorRegistry = new ExtractorRegistry(logger, [new Fembed(new FetcherMock(`${__dirname}/__fixtures__/Fembed`))]);
 
 const ctx = createTestContext({ mediaFlowProxyUrl: 'https://mediaflow-proxy.test', mediaFlowProxyPassword: 'asdfg' });
 
-describe('Mixdrop', () => {
-  test('mixdrop.my /e/', async () => {
-    expect(await extractorRegistry.handle(ctx, new URL('https://mixdrop.my/e/knq0kj8waq44l8'))).toMatchSnapshot();
+describe('Fembed', () => {
+  test('fembed.sx filme dublado', async () => {
+    expect(await extractorRegistry.handle(ctx, new URL('https://fembed.sx/e/671-dub'))).toMatchSnapshot();
   });
 
-  test('deleted or expired file', async () => {
-    expect(await extractorRegistry.handle(ctx, new URL('https://mixdrop.ag/e/123456789'))).toMatchSnapshot();
+  test('fembed.sx série dublada', async () => {
+    expect(await extractorRegistry.handle(ctx, new URL('https://fembed.sx/e/94997-dub/1-7'))).toMatchSnapshot();
+  });
+
+  test('fembed.sx com IMDB', async () => {
+    expect(await extractorRegistry.handle(ctx, new URL('https://fembed.sx/e/tt0241527-dub'))).toMatchSnapshot();
+  });
+
+  test('not found', async () => {
+    expect(await extractorRegistry.handle(ctx, new URL('https://fembed.sx/e/999999999-dub'))).toMatchSnapshot();
   });
 });
+
